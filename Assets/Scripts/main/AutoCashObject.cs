9e2ceb37702094648112c102b44e88f96a5de0ac
czzhang3 <czzhang3@illinois.edu>
2023-03-04 02:04:08 -0500

    Initial commit with some boilerplate

---
+++ Assets/Scripts/main/AutoCashObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoCashObject : GameObject
+{
+    public int income { get; set; }
+    public int cost { get; set; }
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+}
---
+++ Assets/Scripts/main/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public List<GameObject> gameObjects;
+    public int targetFrameRate = 0;
+    private float updateCount = 0;
+    private float fixedUpdateCount = 0;
+    void Awake() {
+        Application.targetFrameRate = targetFrameRate;
+    }
+    // Start is called before the first frame update
+    void Start() {
+        gameObjects = new ArrayList<>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void Pause() {
+        Time.timeScale = 0;
+    }
+
+    void Resume() {
+        Time.timeScale = 1;
+    }
+
+
+    // updates the money amount based on income
+    void updateCashBalance() {
+
+    }
+
+    int calculateCashStep(int steps) {
+        int delta = 0;
+        foreach (GameObject obj in gameObjects) {
+            if (obj is AutoCashObject) {
+                delta += obj.income;
+            }
+        }
+    }
+}
